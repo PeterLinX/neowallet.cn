53364afa3583290b27ce0afc1ab4879f07fb0f63
PeterLinX <18610565179@163.com>
2018-04-26 06:43:23 +0800

    add nodes

--- AntSharesApp/scripts/AntShares/Global.ts
+++ AntSharesApp/scripts/AntShares/Global.ts
@@ -15,8 +15,22 @@ namespace AntShares {
 
 
         //MainNet
-        public static mainHttpNetList: string[] = ["http://seed1.neo.org:10332", "http://seed2.neo.org:10332", "http://seed3.neo.org:10332", "http://seed4.neo.org:10332", "http://seed5.neo.org:10332"];
-        public static mainHttpsNetList: string[] = ["https://seed3.neo.org:10331", "https://seed1.neo.org:10331"];
+        public static mainHttpNetList: string[] = ["http://seed1.redpulse.com:10332",
+                                                   "http://seed2.redpulse.com:10332",
+                                                   "http://seed1.neo.org:10332",
+                                                   "http://seed2.neo.org:10332",
+                                                   "http://seed3.neo.org:10332",
+                                                   "http://seed4.neo.org:10332",
+                                                   "http://seed5.neo.org:10332"
+        ];
+        public static mainHttpsNetList: string[] = ["https://seed1.neo.org:10331",
+                                                    "https://seed3.neo.org:10331",
+                                                    "https://seed2.switcheo.network:10331",
+                                                    "https://seed3.switcheo.network:10331",
+                                                    "https://seed4.switcheo.network:10331",
+                                                    "https://seed1.redpulse.com:10331",
+                                                    "https://seed2.redpulse.com:10331"
+        ];
         //TestNet
         public static testNetList: string[] = ["http://seed1.neo.org:20332", "http://seed2.neo.org:20332", "http://seed3.neo.org:20332", "http://seed4.neo.org:20332", "http://seed5.neo.org:20332"];
 
